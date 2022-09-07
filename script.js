9a6af057681fbcafb71d45c80aeb56131dbd5b32
DanielGoncalves666 <danielgoncalveskd@gmail.com>
2022-09-07 17:57:16 -0300

    Cards e formulario em estágio inicial. Adição de script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function abrirFormulario()
+{
+    document.getElementById("modal").style.display = "block";
+}
+
+function fecharFormulario()
+{
+    document.getElementById("modal").style.display = "none";
+}
