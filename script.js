fd19d612c0b31c09143a832749baf9819843578b
DanielGoncalves666 <danielgoncalveskd@gmail.com>
2022-09-08 00:53:09 -0300

    Formulario readequado.

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function abrirFormulario()
 {
     document.getElementById("modal").style.display = "block";
+    document.getElementById("headerElements").style.display = "none"
 }
 
 function fecharFormulario()
 {
     document.getElementById("modal").style.display = "none";
+    document.getElementById("headerElements").style.display = "block"
 }
