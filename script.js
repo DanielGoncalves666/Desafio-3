f7938bf933673045766ebe89417e9058f831a92e
DanielGoncalves666 <danielgoncalveskd@gmail.com>
2022-09-12 21:30:59 -0300

    Página de receitas.

--- script.js
+++ script.js
@@ -9,3 +9,51 @@ function fecharFormulario()
     document.getElementById("modal").style.display = "none";
     document.getElementById("headerElements").style.display = "block"
 }
+
+function mudaselect(valor){
+    if(valor == 2){
+    const sec1 = document.querySelectorAll('.conteudo');
+    sec1.forEach(element =>{
+        element.style.display = 'none';
+    })
+    const sec2 = document.querySelectorAll('.conteudoing');
+    sec2.forEach(element =>{
+        element.style.display = 'block';
+    })
+    const sec3 = document.querySelectorAll('.conteudoprep');
+        sec3.forEach(element =>{
+            element.style.display = 'none';
+        })
+    }else if(valor == 1){
+        const sec1 = document.querySelectorAll('.conteudo');
+        sec1.forEach(element =>{
+            element.style.display = 'block';
+        })
+        const sec2 = document.querySelectorAll('.conteudoing');
+        sec2.forEach(element =>{
+            element.style.display = 'none';
+        })
+        const sec3 = document.querySelectorAll('.conteudoprep');
+        sec3.forEach(element =>{
+            element.style.display = 'none';
+        })
+    }else if(valor == 3){
+        const sec1 = document.querySelectorAll('.conteudo');
+        sec1.forEach(element =>{
+            element.style.display = 'none';
+        })
+        const sec2 = document.querySelectorAll('.conteudoing');
+        sec2.forEach(element =>{
+            element.style.display = 'none';
+        })
+        const sec3 = document.querySelectorAll('.conteudoprep');
+        sec3.forEach(element =>{
+            element.style.display = 'block';
+        })
+
+
+    }
+
+    
+}
+
