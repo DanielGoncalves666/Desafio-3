4e06d7f0f4e0c9518a6dd832f8623ca5f21ffd00
DanielGoncalves666 <danielgoncalveskd@gmail.com>
2022-09-13 13:28:15 -0300

    Remoção de receitas.html. Adição das quatro receitas. Revisão do css das receitas.

--- script.js
+++ script.js
@@ -11,19 +11,20 @@ function fecharFormulario()
 }
 
 function mudaselect(valor){
+    
     if(valor == 2){
-    const sec1 = document.querySelectorAll('.conteudo');
-    sec1.forEach(element =>{
-        element.style.display = 'none';
-    })
-    const sec2 = document.querySelectorAll('.conteudoing');
-    sec2.forEach(element =>{
-        element.style.display = 'block';
-    })
-    const sec3 = document.querySelectorAll('.conteudoprep');
-        sec3.forEach(element =>{
+        const sec1 = document.querySelectorAll('.conteudo');
+        sec1.forEach(element =>{
             element.style.display = 'none';
         })
+        const sec2 = document.querySelectorAll('.conteudoing');
+        sec2.forEach(element =>{
+            element.style.display = 'block';
+        })
+        const sec3 = document.querySelectorAll('.conteudoprep');
+            sec3.forEach(element =>{
+                element.style.display = 'none';
+            })
     }else if(valor == 1){
         const sec1 = document.querySelectorAll('.conteudo');
         sec1.forEach(element =>{
@@ -50,10 +51,5 @@ function mudaselect(valor){
         sec3.forEach(element =>{
             element.style.display = 'block';
         })
-
-
     }
-
-    
 }
-
